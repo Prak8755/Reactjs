caeb2bd3e22fc63ce46881e21d8fba411bc1b08f
Prakash Singh <prakashsingh@Prakashs-MacBook-Air.local>
2023-02-13 11:43:24 +0530

    first lesson added

---
+++ Script.js
@@ -0,0 +1,19 @@
+// React is js library not framework 
+//study about Babel
+
+
+
+
+const name='Prakash';
+//This is normal java script
+
+
+const FullName=<p>Prakash </p>;
+//This is jsx element- means Html swith js is called JSX 
+
+const header=<h1>Hello js how are you</h1> 
+<p>how are you doing</p>
+//This is not a way to store two elements together , to prevent it we have to write like this 
+const head=<div><p>Hello world</p><h1>I am fine </h1></div>
+
+// <> This is called React fragment if it does not have nay name inside it 
